a50c6824b9b02a991cf4a870465ca70082b6a2c5
Romeu Reif <romeureif10@gmail.com>
2023-06-28 09:10:16 -0700

    Configurado as rotas do usuario.js e att v1/index.js

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,4 +1,6 @@
 // Fica as rotas de todos os models
 const router = require('express').Router;
 
+router.use("/usuarios", require("./usuarios"));
+
 module.exports = router;
--- routes/api/v1/usuarios.js
+++ routes/api/v1/usuarios.js
@@ -1,2 +1,26 @@
 const router = require('express').Router();
 const auth = require('../../auth');
+const UsuarioController = require('../../../controllers/UsuarioController');
+
+const usuarioController = new UsuarioController();
+
+// Pegar todos os usuários / coletar dados
+router.get("/", auth.required, usuarioController.index);
+router.get("/:id", auth.required, usuarioController.show);
+
+// Enviar dados para o servidor
+router.post("/login", usuarioController.login);
+router.post("/registrar", usuarioController.store);
+// Atulizar os dados
+router.put("/", auth.required, usuarioController.update);
+// Deletar
+router.delete("/", auth.required, usuarioController.remove);
+
+// Recuperar senhar
+router.get("/recuperar-senha", usuarioController.showRecovery);
+router.post("/recuperar-senha", usuarioController.createRecovery);
+router.get("/senha-recuperada", usuarioController.shwoCompleteRecovery);
+router.post("/senha-recuperada", usuarioController.completeRecovery);
+
+
+module.exports = router;
